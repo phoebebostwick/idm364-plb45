5248bd2493c6a37b7fd3be819479211d54e2dac7
phoebebostwick <phoebebostwick@gmail.com>
2019-06-10 09:50:00 -0400

    Correct firebase api key and links

---
+++ build/precache-manifest.708f343e90f4a770fda17667abdb8dc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a47b64e092aa3015d1e9",
+    "url": "/idm364-plb45/static/js/runtime~main.a47b64e0.js"
+  },
+  {
+    "revision": "bf4192c6979257429f50",
+    "url": "/idm364-plb45/static/js/main.bf4192c6.chunk.js"
+  },
+  {
+    "revision": "359b136a7d587b17dd51",
+    "url": "/idm364-plb45/static/js/1.359b136a.chunk.js"
+  },
+  {
+    "revision": "bf4192c6979257429f50",
+    "url": "/idm364-plb45/static/css/main.573a6b2d.chunk.css"
+  },
+  {
+    "revision": "acf87062b08313fc0d6d2a963b8e7a99",
+    "url": "/idm364-plb45/index.html"
+  }
+];
--- src/js/base.js
+++ src/js/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: "AIzaSyBxbyssZ629rISw_8EW_kCwX0NfSxsjivE",
-	authDomain: "idm364-plb45.firebaseapp.com",
-	databaseURL: "https://idm364-plb45.firebaseio.com"
+	apiKey: "AIzaSyCR2w8iea3svfc_vHgjYMSxeigtg7y31k8",
+	authDomain: "plb45-64b59.firebaseapp.com",
+	databaseURL: "https://plb45-64b59.firebaseio.com"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
